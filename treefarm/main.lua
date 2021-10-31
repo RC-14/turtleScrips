df50987763e8f7e137d009ad7b2af11a6f723080
OfficerPitri <68754085+OfficerPitri@users.noreply.github.com>
2021-10-31 21:09:06 +0100

    Update main.lua

--- treefarm/main.lua
+++ treefarm/main.lua
@@ -1,2 +1,19 @@
---kmmentar du 
---verrender was
+local sqrdist=3
+local farmx=16 -- (farmx-1)mod3=0
+local farmy=4 -- (farmy-1)mod3=0
+
+while true do
+    if turtle.compare() then
+    turtle.dig()
+    turtle.forward()
+    while turtle.detectUp() do  
+        turtle.digUp()
+        turtle.up()
+    end
+    while turtle.detectDown() do  
+        turtle.down()
+    end
+      turtle.back()
+    end
+end
+
