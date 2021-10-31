c7281944ac9652216fdb9265dda8d62b5dd8e2ac
OfficerPitri <68754085+OfficerPitri@users.noreply.github.com>
2021-10-31 21:35:53 +0100

    Update main.lua

--- treefarm/main.lua
+++ treefarm/main.lua
@@ -1,19 +1,20 @@
 local sqrdist=3
 local farmx=16 -- (farmx-1)mod3=0
 local farmy=4 -- (farmy-1)mod3=0
+destroyTree()
 
-while true do
+function destroyTree() 
     if turtle.compare() then
-    turtle.dig()
-    turtle.forward()
-    while turtle.detectUp() do  
-        turtle.digUp()
-        turtle.up()
-    end
-    while not turtle.detectDown() do  
-        turtle.down()
-    end
-      turtle.back()
+        turtle.dig()
+        turtle.forward()
+        while turtle.detectUp() do  
+            turtle.digUp()
+            turtle.up()
+        end
+        while not turtle.detectDown() do  
+            turtle.down()
+        end
+        turtle.back()
     end
 end
 
