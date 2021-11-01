622fcfe10ada513abee3e657e6cfa50ccbd7d747
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-01 10:38:56 +0100

    add getType function

--- treefarm/main.lua
+++ treefarm/main.lua
@@ -2,6 +2,26 @@ local sqrdist = 3
 local farmx = 16 -- (farmx-1)mod3=0
 local farmy = 4 -- (farmy-1)mod3=0
 
+local function getType(id)
+    if id == 'minecraft:air' then
+        return 'air'
+    elseif id == 'minecraft:sapling' then
+        return 'sapling'
+    end
+
+    local logIDs = {
+        'minecraft:log',
+        'minecraft:log2'
+    }
+    for i = 1, #logIDs, 1 do
+        if id == logIDs[i] then
+            return 'log'
+        end
+    end
+
+    return 'other'
+end
+
 local function placeSapling()
     turtle.select(16)
     turtle.place()
