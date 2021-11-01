128a9c4d7335a4c1647e8005ec660897de81eabc
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-01 10:15:25 +0100

    format with "lua-format-extension" for vscode

--- treefarm/main.lua
+++ treefarm/main.lua
@@ -1,22 +1,22 @@
-local sqrdist=3
-local farmx=16 -- (farmx-1)mod3=0
-local farmy=4 -- (farmy-1)mod3=0
+local sqrdist = 3
+local farmx = 16 -- (farmx-1)mod3=0
+local farmy = 4 -- (farmy-1)mod3=0
 
 local function placeSapling()
-    turtle.select(16) 
-    turtle.place()   
+    turtle.select(16)
+    turtle.place()
     turtle.select(1)
 end
 
-local function destroyTree() 
+local function destroyTree()
     if turtle.compare() then
         turtle.dig()
         turtle.forward()
-        while turtle.detectUp() do  
+        while turtle.detectUp() do
             turtle.digUp()
             turtle.up()
         end
-        while not turtle.detectDown() do  
+        while not turtle.detectDown() do
             turtle.down()
         end
         turtle.back()
@@ -25,5 +25,5 @@ local function destroyTree()
 end
 
 while true do
-    destroyTree()  
+    destroyTree()
 end
