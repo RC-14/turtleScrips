3c63d05f94f27fdad6ed87b1faae5d591d0173b8
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-03 11:39:35 +0100

    add download function

--- EndlessMinig/run.lua
+++ EndlessMinig/run.lua
@@ -18,3 +18,36 @@ local librariesPath = '/libraries'
 	All files in the libraries folder
 	that are required by the program.
 ]] local libraries = {}
+
+local function download(url, path)
+    local response = http.get(url)
+    local status = response.getResponseCode()
+
+    if not (status == 0 or (status >= 200 and status < 400)) then
+        return false, 'http', status -- request was not successfull
+    end
+
+    local file = fs.open(path, 'w')
+
+    if file == nil then
+        return false, 'file', 'read only' -- can't write to that location
+    end
+
+    local contents = response.readAll()
+
+    file.write(contents)
+    file.close()
+
+    file = fs.open(path, 'r')
+    if file == nil then
+        return false, 'file', "can't read" -- can't read from that location
+    end
+
+    if file.readAll() ~= contents then
+        return false, 'file', 'faulty write' -- contents of the file aren't what they should be
+    end
+
+    file.close()
+
+    return true, url, path -- downloaded the file successfully
+end
