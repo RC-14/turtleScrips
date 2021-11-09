584594a3c6652b36085807a19dd8e7020cf3fd63
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-09 23:50:24 +0100

    add show usage function

--- BuildFlatArea/main.lua
+++ BuildFlatArea/main.lua
@@ -2,7 +2,21 @@ local ta = require '/libraries/turtleAdditions'
 
 local length, width, requiredFuelLevel = ...
 
+local function showUsage()
+    local tmp = string.find(string.reverse(shell.getRunningProgram()), '/')
+    local programName = string.sub(shell.getRunningProgram(), -1 * tmp + 1)
+
+    print(programName .. ' length width [requiredFuelLevel]')
+    print()
+    print('width')
+    print('|')
+    print('^--- length')
+    print()
+    print('^ is the turtle (POV: you are above the turtle)')
+end
+
 if length == nil or width == nil then
+    showUsage()
     error('length and width must be set')
 elseif requiredFuelLevel == nil then
     requiredFuelLevel = turtle.getFuelLimit()
@@ -14,6 +28,7 @@ width = math.floor(width)
 requiredFuelLevel = math.floor(requiredFuelLevel)
 
 if length < 1 or width < 1 or requiredFuelLevel < 1 then
+    showUsage()
     error("length, width and requiredFuelLevel can't be lower than 1")
 end
 
