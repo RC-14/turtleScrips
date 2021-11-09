368ac2886dd3e01ba293c659a60abb5b23f4bc7a
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-09 21:03:56 +0100

    add checks and conversion for args

--- BuildFlatArea/main.lua
+++ BuildFlatArea/main.lua
@@ -1,1 +1,15 @@
 local ta = require 'libraries.turtleAdditions'
+
+local length, width = ...
+
+if length == nil or width == nil then
+    error('length and width must be set')
+end
+
+-- conversion to integers
+length = math.floor(length)
+width = math.floor(width)
+
+if length < 1 or width < 1 then
+    error("length and width can't be lower than 1")
+end
