df3bbbf1912774f2fbd0c7d2032022290ec69dd6
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-09 23:48:05 +0100

    add arg 3 for requiredFuelLevel

--- BuildFlatArea/main.lua
+++ BuildFlatArea/main.lua
@@ -1,18 +1,22 @@
 local ta = require '/libraries/turtleAdditions'
 
-local length, width = ...
+local length, width, requiredFuelLevel = ...
 
 if length == nil or width == nil then
     error('length and width must be set')
+elseif requiredFuelLevel == nil then
+    requiredFuelLevel = turtle.getFuelLimit()
 end
 
 -- conversion to integers
 length = math.floor(length)
 width = math.floor(width)
+requiredFuelLevel = math.floor(requiredFuelLevel)
 
-if length < 1 or width < 1 then
-    error("length and width can't be lower than 1")
+if length < 1 or width < 1 or requiredFuelLevel < 1 then
+    error("length, width and requiredFuelLevel can't be lower than 1")
 end
+
 
 for i = 1, width do
     for i = 1, length do
