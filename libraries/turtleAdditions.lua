c471ba00913c50698232ab244405d19a8ae229f1
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-09 19:31:15 +0100

    add placeItem functions

--- libraries/turtleAdditions.lua
+++ libraries/turtleAdditions.lua
@@ -1,6 +1,6 @@
 local ta = {}
 
-function ta.searchInventoryFor(itemID) -- Searches the inventory of the turtle for slots with items that have the given item id and returns a table with the results.
+function ta.searchInventoryFor(itemID) -- Searches the inventory for slots with items that have the given item id and returns a table with the results.
     local slots = {}
 
     for i = 1, 16 do
@@ -42,6 +42,54 @@ function ta.getIdDown() -- Returns the item id of the block below the turtle.
     end
 
     return 'minecraft:air' -- success is false if the block is air
+end
+
+function ta.placeItem(itemID) -- Searches the inventory for the specified item and tries to place it in front of the turtle.
+    local origSlot = turtle.getSelectedSlot()
+    local slots = ta.searchInventoryFor(itemID)
+
+    if #slots == 0 then
+        return false
+    end
+
+    turtle.select(slots[1])
+    local success = turtle.place()
+
+    turtle.select(origSlot)
+
+    return success
+end
+
+function ta.placeItemUp(itemID) -- Searches the inventory for the specified item and tries to place it above the turtle.
+    local origSlot = turtle.getSelectedSlot()
+    local slots = ta.searchInventoryFor(itemID)
+
+    if #slots == 0 then
+        return false
+    end
+
+    turtle.select(slots[1])
+    local success = turtle.placeUp()
+
+    turtle.select(origSlot)
+
+    return success
+end
+
+function ta.placeItemDown(itemID) -- Searches the inventory for the specified item and tries to place it below the turtle.
+    local origSlot = turtle.getSelectedSlot()
+    local slots = ta.searchInventoryFor(itemID)
+
+    if #slots == 0 then
+        return false
+    end
+
+    turtle.select(slots[1])
+    local success = turtle.placeDown()
+
+    turtle.select(origSlot)
+
+    return success
 end
 
 function ta.uTurn(right) -- Let the turtle make a U turn. (if the optional parameter is true it uses turnRight instead of turnLeft)
