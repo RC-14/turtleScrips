35729713c9f357da8f583c62886f017832013ddb
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-11 05:30:42 +0100

    add isEnderChest function

--- EndlessMinig/checkForRequirements.lua
+++ EndlessMinig/checkForRequirements.lua
@@ -5,3 +5,11 @@ local ENERGYCELL_ID = 'thermalexpansion:cell'
 local ENDERCHEST_ID = 'enderstorage:ender_storage'
 local CHUNKLOADER_ID = 'peripheralsplusone:chunk_loader_upgrade'
 local PICKAXE_ID = 'minecraft:diamond_pickaxe'
+
+local function isEnderChest()
+    turtle.place()
+    local state = turtle.inspect().state
+    turtle.dig()
+
+    return state.type == 'ender_chest'
+end
