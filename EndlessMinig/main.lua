2fe3652a4136aafe103acb7e5b8b37cccfaa1f10
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-11 05:45:59 +0100

    throw an error if we don't have the required items

--- EndlessMinig/main.lua
+++ EndlessMinig/main.lua
@@ -6,4 +6,8 @@ local ENDERCHEST_ID = 'enderstorage:ender_storage'
 
 local hasRequiredItems = shell.run('/' .. fs.getDir(shell.getRunningProgram()) .. '/checkForRequirements.lua')
 
+if not hasRequiredItems then
+    error()
+end
+
 turtle.select(1)
