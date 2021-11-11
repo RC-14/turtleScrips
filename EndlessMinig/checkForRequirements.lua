4dc5084ee7d546acfb056d7e8d856db52f78d3b1
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-11 05:29:38 +0100

    add item IDs

--- EndlessMinig/checkForRequirements.lua
+++ EndlessMinig/checkForRequirements.lua
@@ -1,1 +1,7 @@
 local ta = require '/libraries/turtleAdditions'
+
+local TURTLECHARGER_ID = 'peripheralsplusone:rf_charger'
+local ENERGYCELL_ID = 'thermalexpansion:cell'
+local ENDERCHEST_ID = 'enderstorage:ender_storage'
+local CHUNKLOADER_ID = 'peripheralsplusone:chunk_loader_upgrade'
+local PICKAXE_ID = 'minecraft:diamond_pickaxe'
