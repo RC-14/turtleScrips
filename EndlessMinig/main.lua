238ec64702b39435fc99f1acf434751bff4cb22b
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-11 05:27:08 +0100

    create new file for requirements check

--- EndlessMinig/main.lua
+++ EndlessMinig/main.lua
@@ -4,12 +4,6 @@ local TURTLECHARGER_ID = 'peripheralsplusone:rf_charger'
 local ENERGYCELL_ID = 'thermalexpansion:cell'
 local ENDERCHEST_ID = 'enderstorage:ender_storage'
 
-local hasRequiredItems =
-    shell.run(
-    '/' .. fs.getDir(shell.getRunningProgram()) .. '/checkForRequiredItems.lua',
-    TURTLECHARGER_ID,
-    ENERGYCELL_ID,
-    ENDERCHEST_ID
-)
+local hasRequiredItems = shell.run('/' .. fs.getDir(shell.getRunningProgram()) .. '/checkForRequirements.lua')
 
 turtle.select(1)
