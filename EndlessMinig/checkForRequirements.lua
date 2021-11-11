4d11c5a7cd7f7da1350dc32cdf4cac3b19282637
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-11 05:35:16 +0100

    check if the correct items are in the correct slot

--- EndlessMinig/checkForRequirements.lua
+++ EndlessMinig/checkForRequirements.lua
@@ -18,3 +18,39 @@ local function fail(message)
     turtle.drop()
     error(message)
 end
+
+turtle.select(1)
+local item = turtle.getItemDetail()
+
+if item == nil or item.name ~= ENDERCHEST_ID or not isEnderChest() then
+    fail('No item chest.')
+elseif item.count > 1 then
+    turtle.drop(item.count - 1)
+end
+
+turtle.select(2)
+item = turtle.getItemDetail()
+
+if item == nil or item.name ~= TURTLECHARGER_ID then
+    fail('No charger.')
+elseif item.count > 1 then
+    turtle.drop(item.count - 1)
+end
+
+turtle.select(3)
+item = turtle.getItemDetail()
+
+if item == nil or item.name ~= ENERGYCELL_ID then
+    fail('No energy cell.')
+elseif item.count > 1 then
+    turtle.drop(item.count - 1)
+end
+
+turtle.select(4)
+item = turtle.getItemDetail()
+
+if item == nil or item.name ~= ENDERCHEST_ID or not isEnderChest() then
+    fail('No energy chest.')
+elseif item.count > 1 then
+    turtle.drop(item.count - 1)
+end
