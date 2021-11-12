bc8572711dc44a0ae9b80451f62a87bb3b2e2a1e
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-12 01:03:31 +0100

    add variables for important slots

--- EndlessMinig/main.lua
+++ EndlessMinig/main.lua
@@ -4,6 +4,8 @@ local TURTLECHARGER_ID = 'peripheralsplusone:rf_charger'
 local ENERGYCELL_ID = 'thermalexpansion:cell'
 local ENDERCHEST_ID = 'enderstorage:ender_storage'
 
+local ITEMCHEST_SLOT, CHARGER_SLOT, ENERGYCELL_SLOT, ENERGYCHEST_SLOT = 1, 2, 3, 4
+
 local hasRequiredItems = shell.run('/' .. fs.getDir(shell.getRunningProgram()) .. '/checkForRequirements.lua')
 
 if not hasRequiredItems then
