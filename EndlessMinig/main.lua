f3a82e0bf28edbc2061ef7a5923272d55f94362c
RC-14 <61058098+RC-14@users.noreply.github.com>
2021-11-14 00:45:11 +0100

    rename variable

--- EndlessMinig/main.lua
+++ EndlessMinig/main.lua
@@ -1,6 +1,6 @@
 local ta = require '/libraries/turtleAdditions'
 
-local TURTLECHARGER_ID = 'peripheralsplusone:rf_charger'
+local CHARGER_ID = 'peripheralsplusone:rf_charger'
 local ENERGYCELL_ID = 'thermalexpansion:cell'
 local ENDERCHEST_ID = 'enderstorage:ender_storage'
 
